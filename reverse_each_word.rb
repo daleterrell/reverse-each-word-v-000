2fd9c6e8851a296218c2ed5f93083acd7b8f9d40
Dale Terrell <tragicid@gmail.com>
2018-05-06 02:26:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,6 +7,7 @@ require 'pry'
 
 def reverse_each_word(string)
   sentence_array = string.split(" ")
-  sentence_array.collect {|word| word.reverse.join(" ")}
-
+  sentence_array.collect do |w|
+    w.reverse
+  end.join(" ")
 end
